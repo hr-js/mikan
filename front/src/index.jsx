1b6509492cf9a5e1a22aefa0d24b29b816dd26d9
tomoyuki-tanaka <masuminium5@gmail.com>
2017-05-21 23:22:40 +0900

    js build settings

---
+++ front/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+const App = () => <div>Hello, Mikan</div>
+
+ReactDom.render(
+  <App/>,
+  document.getElementById('index')
+)
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,32 @@
 const path = require('path')
+const webpack = require('webpack')
+
 module.exports = {
-  context: __dirname + '/js/src',
+  context: __dirname + '/front/src',
   entry: {
-    login: './login.js',
-    index: './index.js'
+    login: './login.jsx',
+    index: './index.jsx'
   },
   output: {
-    path: path.join(__dirname, '/js/build'),
+    path: path.join(__dirname, '/front/bundle'),
     filename: '[name].js'
   },
   module: {
     rules: [{
       test: /\.jsx?$/,
-      use: 'babel-loader',
-      exclude: '/node_modules/'
+      use: 'babel-loader'
     }]
   },
   resolve: {
     extensions: ['.js', '.jsx']
-  }
+  },
+  devServer: {
+    contentBase: 'front/bundle',
+    inline: true,
+    hot: true
+  },
+  devtool: 'source-map',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 }
